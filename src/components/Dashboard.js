d8e248d7a9cc8710e2246e3b3bf1bfb366912a99
MatejTuray <41123694+MatejTuray@users.noreply.github.com>
2018-09-23 23:36:42 +0200

    f

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -317,9 +317,9 @@ class Dashboard extends React.Component {
                             <h5 className="text-black mt-2">Filters:</h5>
                             <form onSubmit={(e) => this.handleSubmit(e)}>
                                 <div className="form-row align-items-space-around mb-2">
-                                    <div className="col-lg-6 h-100 w-100 mt-2 col-sm-12">
+                                    <div id = "select" className="col-lg-6 h-100 w-100 mt-2 col-sm-12">
                                         <Select
-                                            className="react-select"
+                                           
                                             closeMenuOnSelect={true}
                                             components={makeAnimated()}
                                             options={options}
