ad78493cb3f66a764871e546f4174ad9ac8a38f4
MatejTuray <41123694+MatejTuray@users.noreply.github.com>
2018-09-26 22:25:29 +0200

    Delete config.js

--- src/components/config.js
+++
@@ -1,5 +0,0 @@
-const facebook = "1959464484097415"
-const google = "225917510343-d2mine7afr8rl6l6nfdih15k723me8kj.apps.googleusercontent.com"
-const apiKey = "acd44ebf"
-
-export {facebook, google, apiKey}
